29fac733a397f25700d8134d70366b817b6b5bf3
Kas <cybercyber@runbox.com>
2021-01-21 10:35:01 -0800

    selected curve

--- pytls.py
+++ pytls.py
@@ -17,7 +17,8 @@ bob = Person("bob")
 
 
 
-# Step 1: Elliptic Curve Diffie-Hellman exchange to establish a shared secret using a NIST approved curve.
+# Step 1: Elliptic Curve Diffie-Hellman exchange to establish a shared secret.
+#         This will use the P-256 curve, approved by NIST SP 800-56A Rev 3.
 
 
 
