ff0f0aae1c89739e150a7948b90858515a129f34
Kas <cybercyber@runbox.com>
2021-01-22 16:17:50 -0800

    AES is working

--- pytls.py
+++ pytls.py
@@ -4,12 +4,12 @@
 
 # Libraries
 from tinyec import registry
-import secrets, binascii, os
+import secrets, binascii, os, hashlib
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import scrypt
-from base64 import b64decode,b64encode
+#from Crypto.Protocol.KDF import scrypt
+
 
 # global variables
 
@@ -41,29 +41,37 @@ def create_ecc_key_pairs():
     curve = registry.get_curve('brainpoolP256r1')
     alice.generate_ecc_key_pairs(curve)
     bob.generate_ecc_key_pairs(curve)
-    print("Alice's public ECC key: ", compress(alice.ecc_public_key))
-    print("Bob's public ECC key:  ", compress(bob.ecc_public_key))
 
 def exchange_ecc_public_keys():
     # In reality, this would be through the internet...
     alice.generate_ecc_shared_key(bob.ecc_public_key)
     bob.generate_ecc_shared_key(alice.ecc_public_key)
-    print("Alice's shared ECC key: ", compress(alice.ecc_shared_key))
-    print("Bob's shared ECC key: ", compress(bob.ecc_shared_key))
 
-# Encrypt with AES-GCM and sign with RSA
+# Encrypt with AES-GCM
 def encrypt(plain_text, shared_secret):
-    cipher = AES.new(shared_secret, AES.MODE_GCM)
-    cipher_text = cipher.encrypt(plain_text)
+    aes_key = hashlib.sha256(int.to_bytes(shared_secret.x, 32, 'big'))
+    aes_key.update(int.to_bytes(shared_secret.y, 32, 'big'))
+    cipher = AES.new(aes_key.digest(), AES.MODE_GCM)
+    cipher_text = cipher.encrypt(plain_text.encode())
     return (cipher_text, cipher.nonce)
+
+# Sign with RSA
 
 def create_rsa_key_pairs():
     alice.generate_rsa_key_pairs()
     bob.generate_rsa_key_pairs()
-    print(f"Alice's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
-    print(f"Bob's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
 
-# Decrypt and verify
+# Decrypt with AES-GCM
+def decrypt(encrypted_message, shared_secret):
+    (cipher_text, nonce) = encrypted_message
+    aes_key = hashlib.sha256(int.to_bytes(shared_secret.x, 32, 'big'))
+    aes_key.update(int.to_bytes(shared_secret.y, 32, 'big'))
+    cipher = AES.new(aes_key.digest(), AES.MODE_GCM, nonce)
+    plain_text = cipher.decrypt(cipher_text)
+    return plain_text
+
+
+# Verify with RSA
 
 
 
@@ -73,16 +81,31 @@ def create_rsa_key_pairs():
 
 
 # Main
-print("\nGenerate public/private key pairs and establishing shared secret.")
+print("\nALICE and BOB both generate ECC and RSA public/private key pairs and establish a shared secret.")
 create_ecc_key_pairs()
 create_rsa_key_pairs()
 exchange_ecc_public_keys()
+print("Alice's public ECC key:", compress(alice.ecc_public_key))
+print("Bob's public ECC key: ", compress(bob.ecc_public_key))
+print("Alice's shared ECC key:", compress(alice.ecc_shared_key))
+print("Bob's shared ECC key:", compress(bob.ecc_shared_key))
+print(f"Alice's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
+print(f"Bob's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
 
-print("\nEncrypt and send the message using AES-GCM.")
+
+print("\nALICE encrypts the message using AES-GCM.")
 secret_message = "The attack will happen at dawn!"
-print("Alice's message: ", secret_message)
-# encrypted_message = encrypt(secret_message, compress(alice.ecc_shared_key))
-# print("Encrypted message: ", encrypted_message)
+print("Alice's sends:", secret_message)
+encrypted_message = encrypt(secret_message, alice.ecc_shared_key)
+print("Encrypted message: ", encrypted_message)
 
+print("\nALICE signs it with RSA.")
+# TODO
 
-print("\nVerify and decrypt the message.")
+print("\nALICE sends it to Bob...")
+
+print("\nBOB verifies the RSA signature.")
+
+print("\nBOB decrypts the AES message.")
+decrypted_text = decrypt(encrypted_message, bob.ecc_shared_key)
+print("Bob receives:", decrypted_text.decode())
