0eec5bf3e8a9eefce21c288fd60c3ce1b740c4dc
kasclark <kasclark@runbox.com>
2021-02-27 16:39:25 -0800

    added RSA stuff

--- pytls.py
+++ pytls.py
@@ -4,12 +4,13 @@
 
 # Libraries
 from tinyec import registry
-import secrets, binascii, os, hashlib
+import secrets, binascii, os
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
-
-
-# global variables
+from hashlib import sha256
+from Crypto.Hash import SHA256
+from Crypto.Signature import pss
+from Crypto import Random
 
 # Objects for Alice and Bob
 class Person:
@@ -26,7 +27,7 @@ class Person:
     def generate_ecc_shared_key(self,key):
         self.ecc_shared_key = key * self.ecc_private_key
 
-
+# global variables
 alice = Person("alice")
 bob = Person("bob")
 
@@ -47,36 +48,41 @@ def exchange_ecc_public_keys():
 
 # Encrypt with AES-GCM
 def encrypt(plain_text, shared_secret):
-    aes_key = hashlib.sha256(int.to_bytes(shared_secret.x, 32, 'big'))
+    aes_key = sha256(int.to_bytes(shared_secret.x, 32, 'big'))
     aes_key.update(int.to_bytes(shared_secret.y, 32, 'big'))
     cipher = AES.new(aes_key.digest(), AES.MODE_GCM)
     cipher_text = cipher.encrypt(plain_text.encode())
     return (cipher_text, cipher.nonce)
 
-# Sign with RSA
+# Generate RSA keypairs
 def create_rsa_key_pairs():
     alice.generate_rsa_key_pairs()
     bob.generate_rsa_key_pairs()
 
-
+# RSA sign the message
+def sign(message, key):
+    h = SHA256.new(message.encode())
+    signature = pss.new(key).sign(h)
+    return signature
 
 # Decrypt with AES-GCM
 def decrypt(encrypted_message, shared_secret):
     (cipher_text, nonce) = encrypted_message
-    aes_key = hashlib.sha256(int.to_bytes(shared_secret.x, 32, 'big'))
+    aes_key = sha256(int.to_bytes(shared_secret.x, 32, 'big'))
     aes_key.update(int.to_bytes(shared_secret.y, 32, 'big'))
     cipher = AES.new(aes_key.digest(), AES.MODE_GCM, nonce)
     plain_text = cipher.decrypt(cipher_text)
     return plain_text
 
-
 # Verify with RSA
-
-
-
-
-    
-
+def verify(message, signature):
+    h = SHA256.new(message.encode())
+    verifier = pss.new(alice.RSA_keypair)
+    try:
+        verifier.verify(h, signature)
+        return True
+    except (ValueError, TypeError):
+        return False
 
 
 # Main
@@ -91,21 +97,24 @@ print("Bob's shared ECC key:", compress(bob.ecc_shared_key))
 print(f"Alice's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
 print(f"Bob's public RSA key:  (n={hex(alice.RSA_keypair.n)}, e={hex(alice.RSA_keypair.e)})")
 
-
-print("\nALICE encrypts the message using AES-GCM.")
 secret_message = "The attack will happen at dawn!"
-print("Alice's sends:", secret_message)
+print("Alice's message:", secret_message)
+print("\nALICE signs it with RSA.")
+signature = sign(secret_message, alice.RSA_keypair)
+print("Signature:", signature)
+print("\nALICE encrypts the message using AES-GCM.")
 encrypted_message = encrypt(secret_message, alice.ecc_shared_key)
 print("Encrypted message: ", encrypted_message)
 
-print("\nALICE signs it with RSA.")
-# TODO
-
-print("\nALICE sends it to Bob...")
-
-print("\nBOB verifies the RSA signature.")
-# TODO
+print("\nALICE sends the message and signature to Bob...")
 
 print("\nBOB decrypts the AES message.")
 decrypted_text = decrypt(encrypted_message, bob.ecc_shared_key)
 print("Bob receives:", decrypted_text.decode())
+print("\nBOB verifies the RSA signature.")
+if(verify(decrypted_text.decode(),signature)):
+    print("Signature is valid.")
+else:
+    print("Signature if invalid!")
+
+
